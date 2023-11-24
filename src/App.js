b2735b5849b43f803703cc161f4fbd15832d446c
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2023-11-24 16:37:29 +0900

    ADD : 깃 푸시 위치 확인

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import 'App.css';
 
+
+// 이 파일이 우리의 플레이그라운드이다
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* 여기가 우리의 플레이그라운드이다 */}
+
+    <h1>
+      hello react~~~!!!
+    </h1>
+    <p>
+      hello react
+    </p>
+
+
     </div>
   );
 }
