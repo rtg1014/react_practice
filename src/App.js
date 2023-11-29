7ff41dc86b713aa62338fc7baaee26d134047151
rtg1014 <rtg1014@naver.com>
2023-11-29 18:00:29 +0900

    ADD :프론트 내용중 props 의 예시를 위한 학습

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import 'App.css';
+
+import React from 'react';
 
 
-// 이 파일이 우리의 플레이그라운드이다
-function App() {
-  return (
-    <div className="App">
-    {/* 여기가 우리의 플레이그라운드이다 */}
-
-    <h1>
-      hello react~~~!!!
-    </h1>
-    <p>
-      hello react
-    </p>
-
-
+const ChildShark = (props) => {
+  console.log("props",props);
+  return <div>
+    <div>
+    나는 {props.family_names[1]} 의 손자 입니다.
     </div>
-  );
+  <div>
+      나는 {props.family_names[0]} 의 아들 입니다.
+  </div>
+  </div>
+}
+
+
+const FatherShark = (props) => {
+  // 부모 컴포넌트에서 자식컴포넌트로 전달함
+  const name = "아빠상어 김흥부";
+  const grand_father_name = props.grand_father_props
+  return <ChildShark family_names={[name,grand_father_name]} />;
+}
+
+
+
+const GrandFatherShark = () => {
+  const grand_father = "할배 김놀부"
+  return <FatherShark  grand_father_props = {grand_father}/>
+}
+
+
+
+function App() {
+  return <GrandFatherShark />
 }
 
 export default App;
