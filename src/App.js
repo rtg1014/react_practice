d9c2dcfd8dd64b49229f0bffe277f12d7f65b18d
rtg1014 <rtg1014@naver.com>
2023-11-29 21:19:32 +0900

    ADD : useState 사용 예시 코드 추가

--- src/App.js
+++ src/App.js
@@ -3,13 +3,40 @@
 import React from "react";
 import Layout from "./Layout"
 import Child from "./child"
+import { useState } from "react";
+import { flushSync } from "react-dom";
 
 function App() {
-  const name = "리액트";
-return <Child
-age = {10} name = {name}
->
-  이름
-</Child>
+
+  function onClick() {
+    alert('로그인 되었습니다.');
+  }
+
+  function onChangeName() {
+    setName('이름 변경됨');
+  }
+
+  const [name, setName] = useState('뉴진스의');
+  console.log('name은 ',name)
+
+  
+  return(
+    <>
+
+    <button onClick={function() {
+      alert('로그인 되었습니다.');
+      setName('하입보이요');
+    }}
+    
+    >로그인</button>
+    <br/>
+
+    {name}
+    <br/>
+
+
+    </>
+  ) 
+
 }
 export default App;
